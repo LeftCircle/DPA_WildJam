31bcba3a69a992c93e4fb048cf992da1af2b8e98
RLucas1102 <robenoltl@gmail.com>
2026-02-20 09:25:42 -0500

    Stopped using metadata and used export  instead  to get values from inspector

--- scenes/objects/locked_door/locked_door.gd
+++ scenes/objects/locked_door/locked_door.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
+@export var lock : HitTarget
+@export var move_magnitude : float
+
 enum {OPEN, CLOSED}
 
 @onready var state = CLOSED
-@onready var lock : HitTarget = get_node(self.get_meta("Target"))
-@onready var move_magnitude : float = self.get_meta("Move_Magnitude")
 
 func _ready() -> void:
 	lock.target_hit.connect(_open_door)
 
 func _open_door() -> void:
 	print(move_magnitude)
-	print("This is target:", lock)
+	print(lock)
+	
+	
