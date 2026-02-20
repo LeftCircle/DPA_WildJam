52b7fe4aee74af43304fe40b91fb3aec8c156213
RLucas1102 <robenoltl@gmail.com>
2026-02-20 01:57:28 -0500

    Reworked hit_target; added hit signal and emits on body entered; able to get state of target with _get_state()

--- scenes/objects/hit_target/hit_target.gd
+++ scenes/objects/hit_target/hit_target.gd
@@ -1,12 +1,20 @@
 extends StaticBody2D
+class_name HitTarget
 
+signal target_hit
+
+enum {ACTIVE, DEACTIVE}
+
+@onready var hit_target_area : Area2D = $Area2D
+@onready var state = DEACTIVE
 
 func _ready():
-	var hit_target_area = get_node("Area2D")
-	hit_target_area.connect("body_entered", _on_area2D_body_entered)
-	
-func _process(delta):
-	pass
+	hit_target_area.body_entered.connect(_on_area2D_body_entered)
 
 func _on_area2D_body_entered(body):
 	print("Feather hit target")
+	state = ACTIVE
+	target_hit.emit()
+
+func _get_state():
+	return state
