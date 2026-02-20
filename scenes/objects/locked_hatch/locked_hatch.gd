af7cf976dc617f751dbc8e9d45f462b99d7e622e
RLucas1102 <robenoltl@gmail.com>
2026-02-20 13:23:24 -0500

    Created pivot point for locked hatch

--- scenes/objects/locked_hatch/locked_hatch.gd
+++ scenes/objects/locked_hatch/locked_hatch.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
 @export var lock : HitTarget
+@export var rotation_speed : float
+@export var rotation_amount : float
 
 enum {OPEN, CLOSED}
+
+@onready var pivot_point : Node2D = $PivotPoint
+@onready var state = CLOSED
 
 func _ready() -> void:
 	lock.target_hit.connect(_open_hatch)
 	
+func _process(delta: float) -> void:
+	if(state == CLOSED):
+		if(pivot_point.rotation > -PI/2):
+			pivot_point.rotate(-0.1)
+	
 func _open_hatch():
-	print("Open hatch")
+	pass
