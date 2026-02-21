c27f5feeddd2609131e47b8f28590812ea95f7d8
RLucas1102 <robenoltl@gmail.com>
2026-02-21 01:22:29 -0500

    Reworked chandelier to have external hit target connected to it and then use Richard's move of move_towards; calculating final position to move_towards

--- scenes/objects/chandelier/chandelier.gd
+++ scenes/objects/chandelier/chandelier.gd
@@ -1,21 +1,18 @@
 extends Node2D
 class_name Chandelier
 
-@export var target : HitTarget
-#@export var chain_length : float
-#@export var move_speed : float
+# Drop into scene with a HitTarget and connect it to this object
 
-#enum {ACTIVE, INACTIVE}
+enum {ACTIVE, DEACTIVE}
 
 @onready var base_sprite : Sprite2D = $StaticBody2D/CollisionShape2D/Base
 @onready var base_body : StaticBody2D = $StaticBody2D
-#@onready var base_target_position = base_body.position
-#@onready var base_resting_position = base_body.position
-#@onready var state = INACTIVE
-#@onready var move_direction : Vector2 = Vector2(0, 0)
+@onready var final_position : Vector2
+@onready var state = DEACTIVE
 
-@export var target_position : Node2D
+@export var target : HitTarget
 @export var velocity : float = 128
+@export var chain_length : float = 100
 
 func _ready() -> void:
 	target.target_hit.connect(_lower_chandelier)
@@ -23,27 +20,21 @@ func _ready() -> void:
 	set_physics_process(false)
 
 func _physics_process(delta: float) -> void:
-	#if state == ACTIVE and base_body.position < base_target_position:
-		#base_body.move_and_collide(move_direction * move_speed * delta)
-	base_body.global_position = base_body.global_position.move_toward(target_position.global_position, delta * velocity)
-	if base_body.global_position.distance_squared_to(target_position.global_position) < 0.1:
+	base_body.global_position = base_body.global_position.move_toward(final_position, delta * velocity)
+	if base_body.global_position.distance_squared_to(final_position) < 0.1:
 		set_physics_process(false)
 
 func _lower_chandelier():
-	set_physics_process(true)
-	#if (state == INACTIVE):
-		#print("lower")
-		#state = ACTIVE
-		#
-		#var unit_vector : Vector2 = Vector2(0, 1)
-		#move_direction = unit_vector
-		#
-		#base_target_position += unit_vector * chain_length
-		#
-		#print(base_resting_position.y)
-		#print(base_target_position.y)
-		#set_physics_process(true)
-		#print("HIT!!!")
+	if (state == DEACTIVE) :
+		set_physics_process(true)
+	
+		# Taking base position and adding a lower position to it; chain_length controls
+		# how far down the chandelier moves
+		final_position = base_body.global_position
+		final_position += Vector2(0,1) * chain_length
+		
+		state = ACTIVE
+	
 
 func _play_fire_animations() -> void:
 	for child in base_sprite.get_children():
