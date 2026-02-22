e1ff3a415f285b37c9ae3ebec88a2d09b41ac808
Jingyin <jingyin0515@gmail.com>
2026-02-22 13:24:21 -0500

    Added the transition.

--- singletons/level_driver.gd
+++ singletons/level_driver.gd
@@ -6,6 +6,7 @@ var current_level : Level
 var player : PlayerCharacter
 var npcs : Dictionary[String, NPC] = {}
 var player_starting_state : PlayerStartingState = PlayerStartingState.new()
+var t : Transition
 
 @onready var level_music : AudioStreamPlayer = $LevelMusic
 
@@ -13,7 +14,7 @@ func _ready():
 	player_starting_state.current_feathers = 0
 	level_music.bus = "Music"
 	level_music.play()
-	
+
 
 func register_npc(npc_name : String, _npc : NPC) -> void:
 	npcs[name] = _npc
@@ -22,20 +23,36 @@ func remove_npc(name : String) -> void:
 	pass
 
 func _on_level_exit():
+	
+	TransitionManager.wipe_in()
+	await TransitionManager.finished
+
 	player_starting_state.current_feathers = player.acquired_feathers
 	#get_tree().change_scene_to_packed(current_level.next_level)
 	get_tree().call_deferred("change_scene_to_packed", current_level.next_level)
 	player_starting_state.npcs_saved = player.npcs_saved
 	player_starting_state.doomed = player.doomed
+	
 	get_tree().change_scene_to_packed(current_level.next_level)
+	await get_tree().process_frame
+
+
+	TransitionManager.wipe_out()
 
 func _on_death_area_entered() -> void:
 	print("Death Area entered")
 	player._on_death()
+	
+	TransitionManager.wipe_in()
+	await TransitionManager.finished
+	
 	player.reset_feathers_to(player_starting_state.current_feathers)
+	
 	#player.global_position = current_level.spawn_point.global_position
 	get_tree().call_deferred("reload_current_scene")
-
+	
+	await get_tree().process_frame
+	TransitionManager.wipe_out()
 
 func _on_level_music_finished():
 	level_music.play(0.0)
