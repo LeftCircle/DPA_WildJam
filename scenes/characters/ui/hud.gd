4dd820449c1e22a9d48282ddd252c83dd8749691
RLucas1102 <robenoltl@gmail.com>
2026-02-21 20:35:08 -0500

    Started creating HUD; feathers now stack on each other

--- scenes/characters/ui/hud.gd
+++ scenes/characters/ui/hud.gd
@@ -1,8 +1,23 @@
-extends Control
-class_name PlayerHUD
+extends CanvasLayer
 
+@onready var num_feathers = LevelDriver.player_starting_state.current_feathers
+@onready var hbox : HBoxContainer = $MarginContainer/HBoxContainer
+@onready var hbox_separation : int = hbox.get_theme_constant("separation")
+@onready var button : Button = $Button
 
+@export var featherUIItem : PackedScene
 
-
-func add_feather() -> void:
-	pass
+func _ready() -> void:
+	button.pressed.connect(_on_button_pressed)
+		
+func _on_button_pressed() -> void:
+	_add_feather()
+	num_feathers += 1
+	if (num_feathers > 5):
+		hbox_separation -= 100/num_feathers
+		hbox.set_deferred("theme_override_constants/separation", hbox_separation)
+	
+func _add_feather():
+	var box_item = featherUIItem.instantiate() as PanelContainer
+	hbox.add_child(box_item)
+		
