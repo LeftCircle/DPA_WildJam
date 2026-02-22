2bb89cbe3aefe00f7ea2c7fc61cfa2aedc817e11
RLucas1102 <robenoltl@gmail.com>
2026-02-22 01:18:36 -0500

    Connected timer timeout through script; reset was not working because feather_used was not connected; emitting reset feathers for HUD

--- scenes/characters/players/feather_resetter.gd
+++ scenes/characters/players/feather_resetter.gd
@@ -7,10 +7,14 @@ var _can_reset : bool = false
 
 @onready var timer : Timer = $Timer
 
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
+	player.feather_used.connect(_on_character_feather_used)
 
 func _physics_process(delta):
 	if _can_reset and player.is_missing_feather() and player.is_on_floor():
 		player.reset_feathers_to(player.acquired_feathers)
+		player.reset_feathers.emit()
 
 
 func _on_timer_timeout():
